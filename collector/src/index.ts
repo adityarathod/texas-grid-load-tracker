4831c97e04788dbed9754a408de46a5af7160a10
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-02-16 22:39:51 -0800

    Add file-based persistence

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,11 +1,20 @@
 import { getStatus } from './request'
 import { parseStatus } from './parser'
+import { readDataFile, writeDataFile } from './persist'
 
 async function main() {
   try {
     const html = await getStatus()
     const res = parseStatus(html)
-    console.log(res)
+    const existingData = readDataFile()
+    let newData: any[]
+    if (!existingData) {
+      newData = [res]
+    } else {
+      newData = [...existingData, res]
+    }
+    writeDataFile(newData)
+    console.log('Written data successfully.')
   } catch (err) {
     console.error('Unable to update status.')
     console.error(err)
@@ -13,4 +22,3 @@ async function main() {
 }
 
 main()
-setInterval(() => main(), 15 * 1000)
